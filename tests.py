39e4072174f6524be6c9f55b12eeb491f34db632
Alex Conrad <alexandre.conrad@gmail.com>
2016-02-29 17:13:07 -0800

    test Person.upper_name()

--- tests.py
+++ tests.py
@@ -7,3 +7,9 @@ def test_person_age():
     from playground.models import Person
     person = Person("muzzy", 1980)
     assert person.age == 36
+
+
+def test_person_upper_name()
+    from plauground.models import Person
+    person = Person("muzzy", 1980)
+    assert person.upper_name() == 'MUZZY'
